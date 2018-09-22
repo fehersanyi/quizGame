27a09f6c2c3505766138baf6b4cc1031f81791dc
fehersanyi <papahadacow@gmail.com>
2018-09-22 12:50:25 +0200

    timer added to the game

--- main.go
+++ main.go
@@ -6,29 +6,47 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of question, answer")
+	timeLimit := flag.Int("time limit", 30, "this is the time limit for answers in seconds.")
 	flag.Parse()
 	file, err := os.Open(*csvFilename)
 	if err != nil {
 		exit(fmt.Sprintf("Error opening the file: %s", *csvFilename))
 	}
 	_ = file
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
+
+	reader := csv.NewReader(file)
+	lines, err := reader.ReadAll()
 	if err != nil {
 		exit("Failed to parse the CSV file.")
 	}
 	problems := parseLines(lines)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
+
+problemLoop:
 	for i, problem := range problems {
 		fmt.Printf("Prblem #%d: %s = ", i+1, problem.question)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == problem.answer {
-			correct++
+		answerChannel := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChannel <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nTime is up! You scored %d out of %d\n", correct, len(problems))
+			break problemLoop
+		case answer := <-answerChannel:
+			if answer == problem.answer {
+				correct++
+			}
 		}
 	}
 	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
